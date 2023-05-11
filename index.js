aab638c67ca4e6538a5f5ea503412d71230b31fb
Abdallah M.Shaltout <97531738+abdallah-shaltout@users.noreply.github.com>
2023-05-11 22:53:53 +0200

    update file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send(`<h1>Hello ${req.ip}</h1>`);
+    res.send(`<h1>Hello ${req.socket.remoteAddress}</h1>`);
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
