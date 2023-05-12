890f0f04d79ea808aae5c59ed238d359fd60315b
Abdallah M.Shaltout <97531738+abdallah-shaltout@users.noreply.github.com>
2023-05-12 08:43:08 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const authRouter = require("./routes/auth");
 
 app.get("/", function (req, res) {
     res.send(`<h1>Hello ${req.header("x-forwarded-for")}</h1>`);
 });
+app.use("/auth", authRouter);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
