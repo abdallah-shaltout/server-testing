05ee618678c95805f05539f1098843663b9ab2e8
Abdallah M.Shaltout <97531738+abdallah-shaltout@users.noreply.github.com>
2023-05-12 08:51:04 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const app = express();
 const authRouter = require("./routes/auth");
-const mongoose = require("mongoose");
 
 app.get("/", function (req, res) {
     res.send(`<h1>Hello ${req.header("x-forwarded-for")}</h1>`);
 });
 app.use("/auth", authRouter);
 
-app.listen(process.env.PORT || 3000, async () => {
-    await mongoose.connect("mongodb+srv://abdallahshaltout:admin@cloudcomputing.9tf94rc.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true });
-    mongoose.set("strictQuery", true);
-});
+app.listen(process.env.PORT || 3000);
