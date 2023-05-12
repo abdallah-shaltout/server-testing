ad4951ad9e5f264e2229c83d6bec43bba616f9b0
Abdallah M.Shaltout <97531738+abdallah-shaltout@users.noreply.github.com>
2023-05-12 08:48:53 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const authRouter = require("./routes/auth");
+const mongoose = require("mongoose");
 
 app.get("/", function (req, res) {
     res.send(`<h1>Hello ${req.header("x-forwarded-for")}</h1>`);
 });
 app.use("/auth", authRouter);
 
-app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, async () => {
+    await mongoose.connect(process.env.DATABASE_LINK, { useNewUrlParser: true });
+    mongoose.set("strictQuery", true);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    res.send("<h1>auth route</h1>");
+    res.send(`<h1>auth route the env is ${process.env.USERNAME || "unknown"}</h1>`);
 });
 
 module.exports = router;
