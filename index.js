a6bf750ed01c9bf24be863dd52227c478f690171
Abdallah M.Shaltout <97531738+abdallah-shaltout@users.noreply.github.com>
2023-05-12 15:22:25 +0200

    update db connection file

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const authRouter = require("./routes/auth");
 
 app.get("/", function (req, res) {
-    res.send(`<h1>Hello ${req.header("x-forwarded-for")}</h1>`);
+    // res.send(`<h1>Hello ${req.header("x-forwarded-for")}</h1>`);
+    res.send("Hello From vercel me");
 });
 app.use("/auth", authRouter);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
